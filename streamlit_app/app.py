90411272c7bb73ff6ff9aedfe88ea87248e642d2
Sai Naveen Pulivarthi <sainaveen1209@gmail.com>
2024-10-31 21:51:16 +0530

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -130,10 +130,10 @@ st.set_page_config(
 col1, col2, col3 = st.columns([1, 4, 1])  # Adjust the width ratios as needed
 
 # Left Column - Design or Image Placeholder
-with col1:
-    # Add your design or image here
-    st.image("images/border.png", use_column_width=True)  # Adjust the image source and options
-# Center Column - Main Content
+# with col1:
+#     # Add your design or image here
+#     st.image("images/border.png", use_column_width=True)  # Adjust the image source and options
+# # Center Column - Main Content
 with col2:
     st.markdown(
         """
@@ -186,9 +186,9 @@ with col2:
                 st.write(f"Detected Disease  ➡️  {prediction}")
 
 # Right Column - Design or Image Placeholder
-with col3:
-    # Add your design or image here
-    st.image("images/border.png", use_column_width=True)  # Adjust the image source and options
+# with col3:
+#     # Add your design or image here
+#     st.image("images/border.png", use_column_width=True)  # Adjust the image source and options
 
 
 

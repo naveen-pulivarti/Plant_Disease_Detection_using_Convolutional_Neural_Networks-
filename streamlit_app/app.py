7aece5b5e16c130214b76dd1e9ddb86307dc9526
Sai Naveen Pulivarthi <sainaveen1209@gmail.com>
2024-10-31 23:49:53 +0530

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -132,7 +132,7 @@ col1, col2, col3 = st.columns([1, 4, 1])  # Adjust the width ratios as needed
 # Left Column - Design or Image Placeholder
 with col1:
     # Add your design or image here
-    st.image(os.path.join(os.path.dirname(__file__), 'images', 'border.png'), use_column_width=True)  # Adjust the image source and options
+    st.image(os.path.join(os.path.dirname(__file__), 'images', 'border.jpg'), use_column_width=True)  # Adjust the image source and options
 # Center Column - Main Content
 with col2:
     st.markdown(
